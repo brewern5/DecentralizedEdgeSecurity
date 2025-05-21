1c5d4242e9c505b52ff7699bd1ff6dcd9ec20072
Nate Brewer <119635918+brewern5@users.noreply.github.com>
2025-05-20 21:48:05 -0400

    Add files via upload

---
+++ Dev/Module/config/Config.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ *              Configuration for each of the nodes in the hierarchy. Will grab machine IP address and will try to generate avaiable ports.
+ *              From ./config/config.properties
+ */
+package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;            // Used for grabbing the machine's IP address
+import java.net.UnknownHostException;   // Error for trying to grab IP address
+
+import java.util.Properties;            // Utility for getting properties from any .properties file
+
+public class Config {
+
+    private static Properties properties = new Properties();      // Generate the properties object
+
+    static {
+        try {
+            FileInputStream in = new FileInputStream("./config/config.properties");
+            properties.load(in);
+            in.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     *   Grabs the machines IP and will return the IP.
+     *   @return a string of the IP if found, if not found then a Null value
+     */
+
+    public String grabIP() throws UnknownHostException {
+
+        String ipAddress = null;
+        
+        InetAddress localHost = InetAddress.getLocalHost();     // Grabs the IP and will convert it to a Java object
+        ipAddress = localHost.getHostAddress();                 // Generates the IP as a string to pass it back to the edge node using it
+
+        return ipAddress;       // Will return a null value if no IP is found 
+    }
+    
+
+    /**
+     *  Will try and grab available port for the machine.    
+     *  @param key - Which node is trying to grab the port
+     *  @return int, if port is found. If not found 0
+     */
+
+    public int generatePort(String key){
+
+        int port = 0;
+        try{
+            port = Integer.parseInt(properties.getProperty(key));
+
+        } catch (Error e){
+            System.err.println("Error getting port from config file!\n");
+            e.printStackTrace();
+        }
+        return port;
+    }
+}
---
+++ Dev/Server/Edge_Server/EdgeServer.java
@@ -0,0 +1,86 @@
+package Edge_Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import config.Config;       // The configuration file for the entire network
+
+public class EdgeServer {
+
+    private static String IP;
+    private static int port;
+
+    private static ServerSocket listening;      // The socket that will be listening to requests from the Edge server.
+    private static Socket sending;              // The socket that will send messages to edge server
+
+    private static Socket edgeModule;
+
+    /*
+     *  Initalizes the Node Module - This will (probably) be the first thing that runs when the Node Module is started up
+     * 
+     */
+
+    public static void init() {
+
+        Config config = new Config();
+
+        // try/catch to generate the IP from ./Config.java - Throws UnknownHostException if it cannot determine the IP
+        try{
+            IP = config.grabIP();
+        } catch (UnknownHostException e) {
+            System.err.println("Error: Unable to determine local host IP address.");
+            e.printStackTrace();
+        }
+
+        port = config.generatePort("edgeServer.port");
+
+        try {
+            edgeModule = new Socket(IP, 4001);
+
+            PrintWriter output = new PrintWriter(edgeModule.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(edgeModule.getInputStream()));
+
+            output.println("Hello From the client!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        // Try to create a serverSocket to listen to requests 
+        try {
+            listening = new ServerSocket(port);
+            System.out.println("Listening on port " + port);
+        } catch (Exception e) {
+            System.err.println("Error creating Listening Socket on port " + port);
+            e.printStackTrace();
+            // TODO: try to grab new port if this one is unavailable
+        }
+        // The sending port. Will come back to this
+        /* 
+        try (Socket socket = new Socket(IP, port);
+             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            String message = "Hello from Edge Module!";
+            output.println(message);
+            System.out.println("Edge Module sent: " + message);
+
+            // If server replies
+            // String response = input.readLine();
+            // System.out.println("Edge Module received: " + response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+    }
+    public static void main(String[] args) {
+
+        init();         // Begins the initalization process 
+
+        
+    }
+}
