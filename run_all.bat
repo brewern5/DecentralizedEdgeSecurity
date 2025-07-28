591ee8dc5b3ba7b0075793709fe17a3f4e907879
Nate Brewer <119635918+brewern5@users.noreply.github.com>
2025-07-28 15:12:30 -0400

    Added maven to help manage this project

--- run_all.bat
+++ run_all.bat
@@ -5,30 +5,36 @@ setlocal
 REM Base directory = where this script is located
 set BASEDIR=%~dp0
 
-REM Path to Gson.jar
-set GSON_JAR=%BASEDIR%\dev\lib\gson-2.13.1.jar
+REM Use Maven to copy dependencies into dev\lib folder
+call mvn dependency:copy-dependencies -DoutputDirectory=%BASEDIR%\dev\lib
+echo Finished maven dependencies commands.
+pause
 
 REM Delete old class files from output directories
 del /S /Q %BASEDIR%\dev\*.class > $null
 
+echo Finished deleting old Class files.
+pause
+
 REM Compile Coordinator
 cd /d %BASEDIR%\dev\coordinator
-javac --release 17 -cp ".;%GSON_JAR%" edge_coordinator\EdgeCoordinator.java
+javac --release 17 -cp ".;%BASEDIR%\dev\lib\*" edge_coordinator\EdgeCoordinator.java
 
 REM Compile Server
 cd /d %BASEDIR%\dev\server
-javac --release 17 -cp ".;%GSON_JAR%" edge_server\EdgeServer.java
+javac --release 17 -cp ".;%BASEDIR%\dev\lib\*" edge_server\EdgeServer.java
 
 REM Compile Node
 cd /d %BASEDIR%\dev\node
-javac --release 17 -cp ".;%GSON_JAR%" edge_node\EdgeNode.java
+javac --release 17 -cp ".;%BASEDIR%\dev\lib\*" edge_node\EdgeNode.java
+echo Finished compiling files.
 pause
 
 REM Run all in separate terminals
 cd /d %BASEDIR%\
 
-start cmd /k "cd /d dev\coordinator && java -cp .;%GSON_JAR% edge_coordinator.EdgeCoordinator"
-start cmd /k "cd /d dev\server && java -cp .;%GSON_JAR% edge_server.EdgeServer"
-start cmd /k "cd /d dev\node && java -cp .;%GSON_JAR% edge_node.EdgeNode"
+start cmd /k "cd /d dev\coordinator && java -cp .;%BASEDIR%\dev\lib\* edge_coordinator.EdgeCoordinator"
+start cmd /k "cd /d dev\server && java -cp .;%BASEDIR%\dev\lib\* edge_server.EdgeServer"
+start cmd /k "cd /d dev\node && java -cp .;%BASEDIR%\dev\lib\* edge_node.EdgeNode"
 
 endlocal
